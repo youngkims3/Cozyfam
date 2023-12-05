faaac660405cf2327108bf8239386ea9a0d2b8b8
youngkims3 <144183126+youngkims3@users.noreply.github.com>
2023-12-05 14:48:35 -0500

    Delete Gallery2.js

--- Gallery2.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const imageStack = document.querySelector('.image-stack');
-
-    imageStack.addEventListener('click', function () {
-        // Toggle the 'expanded' class on click
-        this.classList.toggle('expanded');
-    });
-});
